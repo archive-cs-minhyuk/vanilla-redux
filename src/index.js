d8be77666863d708a0aaf266d8e3c4b7eca3d754
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-30 16:19:28 +0900

    dispatch&subscribe

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  //state=0 준 것은 default 값.
-  return state;
-}; //reducer는 내 data를 modify하는 함수! return하는 것: 내 app에 있는 data
-const countStore = createStore(countModifier); //createStore은 reducer(함수)를 인자로 받아야함.
+number.innerText = 0;
 
-console.log(countStore.getState()); //이렇게 값 사용. 위에서 return한 값 나옴.
+const countModifier = (count = 0, action) => {
+  //state를 modify하는 방법? action 사용!
+  if (action.type === "add") {
+    return count + 1;
+  } else if (action.type === "minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+const countStore = createStore(countModifier);
 
-/* only reducer can modify the state! */
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: "add" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "minus" }));
