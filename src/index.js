18616eabe75b329cec2fb557310c8322d86d9c47
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-30 16:38:16 +0900

    better using switch loop and variables

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   //state를 modify하는 방법? action 사용!
-  if (action.type === "add") {
-    return count + 1;
-  } else if (action.type === "minus") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
@@ -23,5 +27,5 @@ const onChange = () => {
 };
 countStore.subscribe(onChange);
 
-add.addEventListener("click", () => countStore.dispatch({ type: "add" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "minus" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
