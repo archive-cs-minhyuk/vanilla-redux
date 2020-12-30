a28cbd618739206054c3a13785db6c99a915c326
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-30 15:39:58 +0900

    vanilla counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import { createStore } from "redux"; //store는 내 data(state)를 넣는 곳
 
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+//const reducer = () => {};
+//const store = createStore(reducer); //createStore은 reducer(함수)를 인자로 받아야함.
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
