d68180e9360e01d55007b78bad2ddb191838c9c2
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-31 10:59:46 +0900

    basic add_todo

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 const reducer = (state = [], action) => {
-  //never mutate state?
+  //never mutate state? state를 수정할 수 있는 유일한 방법은 action을 dispatch하는 것!
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }]; //...state : es6 spread. 안에 있는 원소들 다 풀어놓는 것
     case DELETE_TODO:
       return [];
     default:
       return state;
-  }
+  } //reducer의 핵심 - state를 mutate하는 것이 아니라 새로운 state를 return하는 것!
 };
 
 const store = createStore(reducer); //기존의 toDo는 html만 바꾸는 꼴이고, toDo list의 원소들을 보관할 수 없었다는 점을 개선!
