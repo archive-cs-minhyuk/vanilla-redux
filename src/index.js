0ee0885a46bcac01384c15f7a32ddf75f292bad7
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-31 16:18:33 +0900

    mapStateToProps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  //store를 index에 연결? ->advantages 있다.
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
