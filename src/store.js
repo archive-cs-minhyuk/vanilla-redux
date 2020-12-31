9c137e629b3404fc167d5dfb44525c49073feac9
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-31 19:48:08 +0900

    made it simple

--- src/store.js
+++ src/store.js
@@ -1,16 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-/* const addToDo = createAction("ADD"); //addToDo 자체는 fucntion임.
-const deleteToDo = createAction("DELETE");
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
-}); //createReducer는 새로운 state를 return해도 되고, 원래의 state를 mutate해도 됨. */
-
 const toDos = createSlice({
   name: "toDosReducer",
   initialState: [],
